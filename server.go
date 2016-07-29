01113c8a65aa736351899771635b05659f97c6b2
Dan Lindeman <lindemda@gmail.com>
2016-07-28 22:23:21 -0400

    give it a first shot...second still validates /second/

--- server.go
+++ server.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-    "io"
+    "fmt"
     "net/http"
+    "strings"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "Hello world!")
+func handler(w http.ResponseWriter, r *http.Request) {
+    var to_validate string = r.URL.Path[1:]
+    fmt.Fprintf(w, "Validating: %s \n\n", to_validate)
+    validate(w, to_validate)
+}
+
+func validate(w http.ResponseWriter, to_validate string) {
+    input := strings.Split(to_validate, "/")
+    var kind, value = input[0], input[1]
+    if strings.Contains("first", kind) {
+        validate_first_kind(w, value)
+    } else {
+        validate_second_kind(w, value)
+    }
+}
+
+func validate_first_kind(w http.ResponseWriter, value string){
+    if strings.Contains("cats", value) {
+        fmt.Fprintf(w, "Super Valid!")
+    } else {
+        fmt.Fprintf(w, "Not Valid!")
+    }
+}
+
+func validate_second_kind(w http.ResponseWriter, value string){
+    if strings.Contains("dogs", value) {
+        fmt.Fprintf(w, "Super Valid!")
+    } else {
+        fmt.Fprintf(w, "Not Valid!")
+    }
 }
 
 func main() {
-    http.HandleFunc("/", hello)
-    http.ListenAndServe(":8000", nil)
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
 }
